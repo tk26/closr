80f55d2cdada0dd1772efaa729ab9d231fd2b9c0
Austin Aryain <austin@acculynx.com>
2019-11-02 17:48:27 -0500

    some changes

--- server/i18n.config.js
+++ server/i18n.config.js
@@ -11,23 +11,7 @@ const i18n = require("i18n"),
 
 i18n.configure({
   locales: [
-    "en_US",
-    "fr_FR",
-    "es_ES",
-    "es_LA",
-    "pt_BR",
-    "id_ID",
-    "ar_AR",
-    "de_DE",
-    "it_IT",
-    "ja_JP",
-    "ko_KR",
-    "ru_RU",
-    "th_TH",
-    "vi_VN",
-    "zh_CN",
-    "zh_HK",
-    "zh_TW"
+    "en_US"
   ],
   defaultLocale: "en_US",
   directory: path.join(__dirname, "locales"),
